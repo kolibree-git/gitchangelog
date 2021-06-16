4c3f70c88009c310b54071f2a9fb8ec7f47318a6
Matthias64 <matthias.larre@gmail.com>
2021-06-16 10:51:13 +0200

    [KLTB002-16077] Linting

--- src/gitchangelog/gitchangelog.py
+++ src/gitchangelog/gitchangelog.py
@@ -1277,7 +1277,8 @@ def kolibree_output(data: dict, opts: dict = {}) -> Generator[str, None, None]:
         return "#" * level + " " + label.strip() + "\n"
 
     def render_version(version: dict) -> str:
-        title = "[%s] %s (%s)" % (version["package"] or "", version["tag"], version["date"]) if version["tag"] else opts["unreleased_version_label"]
+        title = "[%s] %s (%s)" % (version["package"] or "", version["tag"], version["date"]) \
+            if version["tag"] else opts["unreleased_version_label"]
         s = render_title(title, level=2)
 
         sections = version["sections"]
